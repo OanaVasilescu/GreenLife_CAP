58f589515a2286440ebd5e443f9019f10954ddab
oana.vasilescu <oana.vasilescu@sap.com>
2022-03-21 14:23:18 +0200

    url fix

--- app/greenlife/webapp/utils/URLs.js
+++ app/greenlife/webapp/utils/URLs.js
@@ -1,11 +1,12 @@
 sap.ui.define([], function () {
     "use strict";
+    const origin = "http://localhost:4004";
     const adminService = "/admin";
-    const userService = "greenlife";
+    const userService = "/greenlife";
     const products = "/Products";
     return {
         getAllProducts: function () {
-            return userService + products;
+            return origin + userService + products;
         }
     }
 });

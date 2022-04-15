a5ecac01c909c36018214183208ff52a3bee10bb
oana.vasilescu <oana.vasilescu@sap.com>
2022-04-15 11:18:12 +0300

    overview looks ok

--- app/greenlife/webapp/controller/Overview.controller.js
+++ app/greenlife/webapp/controller/Overview.controller.js
@@ -16,10 +16,24 @@ sap.ui.define([
             this.getView().byId("bckgImageWaterOn").setSrc(this.getOwnerComponent().getManifestObject().resolveUri("pictures/water-texture.jpg"));
             this.getView().byId("bckgImageWaterAfter").setSrc(this.getOwnerComponent().getManifestObject().resolveUri("pictures/water-texture.jpg"));
 
+            this.getView().byId("bckgImageLandBefore").setSrc(this.getOwnerComponent().getManifestObject().resolveUri("pictures/land-texture.jpg"));
+            this.getView().byId("bckgImageLandOn").setSrc(this.getOwnerComponent().getManifestObject().resolveUri("pictures/land-texture.jpg"));
+            this.getView().byId("bckgImageLandAfter").setSrc(this.getOwnerComponent().getManifestObject().resolveUri("pictures/land-texture.jpg"));
+
+            this.getView().byId("bckgImageMountainBefore").setSrc(this.getOwnerComponent().getManifestObject().resolveUri("pictures/mountain-texture.jpg"));
+            this.getView().byId("bckgImageMountainOn").setSrc(this.getOwnerComponent().getManifestObject().resolveUri("pictures/mountain-texture.jpg"));
+            this.getView().byId("bckgImageMountainAfter").setSrc(this.getOwnerComponent().getManifestObject().resolveUri("pictures/mountain-texture.jpg"));
+
             this.getView().byId("logoImage").setSrc(this.getOwnerComponent().getManifestObject().resolveUri("pictures/dark-logo.png"));
+
+            this.getView().byId("seeRecyclingMapBox").attachEvent("press", (oEvent) => {
+                this.pressSeeMap()
+            })
+
         },
 
-        pressSearchProduct: function () {
+        pressSearchProduct: function (oEvent) {
+            debugger
             this.getRouter().navTo("SearchProduct");
         },
         pressSeeMap: function () {

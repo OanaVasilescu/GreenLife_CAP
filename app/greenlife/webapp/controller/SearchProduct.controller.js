6d7773f58021176a0a48ede5119afd00c3edda5a
oana.vasilescu <oana.vasilescu@sap.com>
2022-04-28 10:12:38 +0300

    moved pic to url - view

--- app/greenlife/webapp/controller/SearchProduct.controller.js
+++ app/greenlife/webapp/controller/SearchProduct.controller.js
@@ -9,76 +9,9 @@ sap.ui.define([
     return BaseController.extend("greenlife.controller.SearchProduct", {
 
         onInit: function () {
-            sap.ui.getCore().byId("container-webapp---App--app").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"))
+            sap.ui.getCore().byId("container-webapp---App--app").setBackgroundImage("https://images.unsplash.com/photo-1530177150700-84cd9a3b059b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80")
 
-            this.getView().byId("searchTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("scanTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("paperAndCardboard").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("plasticAndPet").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("electro").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("metalAndAlu").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("glass").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("dangerous").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("automoto").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("constructions").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("wood").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("textile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("others").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("paperTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("cardboardTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("boxesTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("bottleTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("bulbsTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("batteriesTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("mobilesTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("largeApplianceTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("smallApplianceTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("zincTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("leadTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("steelTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("stainlessTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("ironTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("brassTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("cansTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("capsTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("aerosolTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("aluFoilTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("aluCansTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("glassTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("masksTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("fertilizerTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("herbicidesTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("pesticidesTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("paintTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("solventTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("medicineTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("windshieldTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("eofTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("oilTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("carBatteriesTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("polystyreneTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("demWoodTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("bricksTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("palletsTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("sawdustTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("furnitureTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("clothesTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("preProdTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("bagsTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("shoesTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("otherTextilesTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("toysTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("organicTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("meshTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("tetraPakTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("foodOilTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("opticTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("waterFiltersTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("absTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("tireTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("petTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-            this.getView().byId("printerTile").setBackgroundImage(this.getOwnerComponent().getManifestObject().resolveUri("pictures/wallBackground.jpg"));
-
+            this.getView().setModel(new JSONModel({backgroundPicture: "https://images.unsplash.com/photo-1530177150700-84cd9a3b059b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"}), "pictureModel")
             this.getView().setModel(new JSONModel({latestSubcategory: null, latestCategory: null}), "chosenModel");
         },
 
@@ -188,7 +121,12 @@ sap.ui.define([
             switch (id) {
                 case "searchTile":
                     this.byId("introStep").setNextStep(this.getView().byId("categoriesWizardStep"));
-                    this.clearCategoriesAndReturnIsAlreadyChosen();
+                    if (latestCategory != null) {
+                        latestCategory.removeStyleClass("pressedButton");
+                    }
+                    if (latestSubcategory != null) {
+                        latestSubcategory.removeStyleClass("pressedButton");
+                    }
                     break;
                 case "scanTile":
                     this.byId("introStep").setNextStep(this.getView().byId("scanStep"));
@@ -246,17 +184,6 @@ sap.ui.define([
             picture.addStyleClass("coverTile");
         },
 
-        clearCategoriesAndReturnIsAlreadyChosen: function () {
-            let isCatAlreadyChosen = false
-            this.getView().byId("categoriesWizardStep").getContent()[0].getItems().forEach(box => box.getItems().forEach(el => {
-                if (el.hasStyleClass("pressedButton")) {
-                    isCatAlreadyChosen = true
-                    el.removeStyleClass("pressedButton")
-                }
-            }))
-            return isCatAlreadyChosen
-        },
-
         goToScanResult: function (oResult) {
             debugger;
         },
@@ -291,6 +218,8 @@ sap.ui.define([
 
             this.getView().getModel("instructionsModel").setProperty("/title", instr.name);
         },
+
+
         getInstructions: async function (subcategory) {
             return await this.get(URLs.getInstructionsBySubcategory(subcategory)).then(async instructionsData => {
                 return instructionsData;
